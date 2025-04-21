3f06ef3a5e7bac961b0a5073bc84898aa3a2db04
jioginting <anakcingkes@gmail.com>
2025-04-21 21:04:26 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import JejakNusantara from './JejakNusantara';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<JejakNusantara />);
